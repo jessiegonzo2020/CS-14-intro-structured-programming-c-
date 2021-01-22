4aee1f56a1eba2fe9fc0408f964152edb5a4774e
Jessie <jessie@indeed.com>
2021-01-21 20:50:47 -0800

    Lab 1 Backup

---
+++ Lab 1 - Jan 21 2021/hello.cpp
@@ -0,0 +1,18 @@
+//
+// hello.cpp
+//
+//Jessie Gonzalez
+//Jan 21, 2021
+//First Lab Assignment for CS14
+//This program prints Hello World to the user
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World, its a beautiful evening!" << endl;
+    return 0;
+
+}
